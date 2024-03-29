e4dfc17b40df372c4372fba23004f27414bf2660
JJB03 <160222162+JJB03@users.noreply.github.com>
2024-03-29 10:20:42 +0900

    LoginController.java
    jjb

--- java_board/src/main/java/com/team2/Controller/LoginController.java
+++ java_board/src/main/java/com/team2/Controller/LoginController.java
@@ -1,12 +1,28 @@
 package com.team2.Controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
 
 import com.team2.App;
+import com.team2.DTO.Board;
+import com.team2.Service.BoardService;
+import com.team2.Service.BoardServiceImpl;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TextField;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+
 
 public class LoginController {
 
@@ -14,6 +30,20 @@ public class LoginController {
     private TextField tid;
     @FXML
     private TextField tpwd;
+    @FXML
+    private Stage stage;
+    @FXML
+    private Scene scene;
+    @FXML
+    private Parent root;
+    
+  
+     // 필요한 객체 생성
+	static List<Board> boardList = null; 		// 게시글 목록
+
+	// 비지니스 로직 계층의 객체 생성
+	static BoardService boardService = new BoardServiceImpl();
+
 
     @FXML
     void moveToJoin(ActionEvent event) throws IOException {
@@ -22,6 +52,29 @@ public class LoginController {
 
     @FXML
     void moveToList(ActionEvent event) throws IOException {
-        App.setRoot("board/list");
+        System.out.println("");
+        login();
     }
-}
+
+    public void login() throws IOException{
+        //변수로 받아오기
+        String id = tid.getText();
+        String pw = tpwd.getText();
+
+        int count = boardService.login(id,pw);
+        if(count != 0){
+            System.err.println("로그인 성공");
+            App.setRoot("board/list"); 
+        }
+        else {
+            System.out.println("로그인 실패");
+            // 경고창 띄우기
+                Alert alert = new Alert(AlertType.WARNING);
+                alert.setTitle("경고창");
+                alert.setHeaderText("아이디/비밀번호가 틀렸습니다.");
+                alert.setContentText("다시 입력해주세요.");
+                alert.showAndWait();
+                }
+            }
+        }
+    
